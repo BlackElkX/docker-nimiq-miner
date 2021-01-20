914c10e9202c723a8dac41f66cb16cf719ed7d78
Pascal MIETLICKI <p.mietlicki@gmail.com>
2021-01-20 18:49:32 +0100

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,9 @@
 
 set -e
 
-/usr/share/nimiq/app/node /usr/share/nimiq/app/index.js --protocol=${PROTO} --miner --pool=${POOL} --type=${TYPE} --wallet-address=${WALLADDR}
+if [ "$PROTO" = "ws" ]
+then /usr/share/nimiq/app/node /usr/share/nimiq/app/index.js --protocol=ws --host={$HOSTNAME} --port=${PORT} --miner --pool=${POOL} --type=${TYPE} --wallet-address=${WALLADDR}
+else /usr/share/nimiq/app/node /usr/share/nimiq/app/index.js --protocol=dumb --miner --pool=${POOL} --type=${TYPE} --wallet-address=${WALLADDR}
+fi
 
 exec "$@"
